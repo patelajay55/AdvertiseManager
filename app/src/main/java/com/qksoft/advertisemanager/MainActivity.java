9f6b645ebefe94a4b458da011da549d2c765cc54
Ajay <patel.ajay55@gmail.com>
2020-02-18 13:32:00 +0530

    Second Commit

--- app/src/main/java/com/qksoft/advertisemanager/MainActivity.java
+++ app/src/main/java/com/qksoft/advertisemanager/MainActivity.java
@@ -1,16 +1,24 @@
 package com.qksoft.advertisemanager;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    String appId = "ca-app-pub-4906547187531410~1017165400";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +27,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        SetUpAdvertisingSdk(appId);
     }
 
+    private void SetUpAdvertisingSdk(String appId) {
+        /**
+         * Don't Forget to add in Application Tag in Manifest
+         * <meta-data
+         *         android:name="com.google.android.gms.ads.APPLICATION_ID"
+         *         android:value="[ADMOB_APP_ID]"/>
+         */
+        MobileAds.initialize(MainActivity.this, appId);
+
+    }
+
+    public AdView createBanner(Context context, AdSize adSize, String bannerId) {
+        AdView adView = new AdView(context);
+        adView.setAdSize(adSize);
+        adView.setAdUnitId(bannerId);
+        adView.loadAd(requestAd());
+        return adView;
+    }
+    public AdRequest requestAd(){
+        return new AdRequest.Builder().build();
+    }
 }
